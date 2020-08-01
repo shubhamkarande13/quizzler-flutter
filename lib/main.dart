ab93b664d007751a1c82b8204cafc15c8e3176bb
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-02 00:47:22 +0530

    show next question

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,15 @@ class _QuizPageState extends State<QuizPage> {
       Icons.close,
       color:Colors.red,
     )
-  ]
+  ];
+  List <String> questions =[
+    'You can lead a cow down stairs but not up stairs.',
+  'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+
+
+  ];
+  int questionNumber=0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,6 +80,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  questionNumber++;
+                });
                 //The user picked true.
               },
             ),
@@ -91,6 +102,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
